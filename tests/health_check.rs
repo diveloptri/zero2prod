7fd0aed827971cbbe74dc1cfd818df18ced747fe
diveloptri <diveloptri@gmail.com>
2024-06-21 20:08:45 +0200

    fix(ci): fix file

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,5 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
-use sqlx::{PgConnection, Connection};
 use zero2prod::configuration::get_configuration;
 
 fn spawn_app() -> String {
