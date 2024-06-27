5e5af2eadca14ea8e72900f3787dd0b9a02fbf15
dev <diveloptri@gmail.com>
2024-06-27 13:55:47 +0200

    refactor(fmt): run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,8 @@ async fn main() -> Result<(), std::io::Error> {
         PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
             .expect("Failed to connect to postgres.");
 
-    let address = format!("{}:{}",
+    let address = format!(
+        "{}:{}",
         configuration.application.host, configuration.application.port
     );
     let listener = TcpListener::bind(address)?;
