5df6f1c26ea636bb75294f0df2d78a5cb891f5e6
diveloptri <diveloptri@gmail.com>
2024-10-06 15:24:15 +0200

    feat(session): introduce typed session

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -9,6 +9,8 @@ use sqlx::PgPool;
 use crate::authentication::AuthError;
 use crate::authentication::{validate_credentials, Credentials};
 use crate::routes::error_chain_fmt;
+use crate::session_state::TypedSession;
+
 use actix_web::http::StatusCode;
 use actix_web_flash_messages::FlashMessage;
 
@@ -25,7 +27,7 @@ pub struct FormData {
 pub async fn login(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
-    session: Session,
+    session: TypedSession,
 ) -> Result<HttpResponse, InternalError<LoginError>> {
     let credentials = Credentials {
         username: form.0.username,
@@ -39,7 +41,7 @@ pub async fn login(
             tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
             session.renew();
             session
-                .insert("user_id", user_id)
+                .insert_user_id(user_id)
                 .map_err(|e| login_redirect(LoginError::UnexpectedError(e.into())))?;
             Ok(HttpResponse::SeeOther()
                 .insert_header((LOCATION, "/admin/dashboard"))
